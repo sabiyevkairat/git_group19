1b1993ff2fbe2c88973c71d3b137a2a3957b72c3
Guzel Khisamieva <guzel.khisamieva89@gmail.com>
2022-08-26 13:10:06 -0400

    Guzel - remove given Char from String

--- src/my_utils/StringUtil.java
+++ src/my_utils/StringUtil.java
@@ -4,4 +4,23 @@ public class StringUtil {
     /*
         This class will be used to create String utility methods
      */
+
+    public static String removeCharacterFromString(String str, char a){
+        String result="";
+
+        for (int i=0; i<str.length(); i++){
+
+            if (str.charAt(i)!=a){
+                result+=str.charAt(i);
+            }
+        }
+
+        return result;
+    }
+
+
+
+
+
+
 }
