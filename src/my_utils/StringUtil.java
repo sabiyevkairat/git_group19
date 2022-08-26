400679d430ee4bc3f519a8df8e2dc1f72ef9dff1
Gentian <gentian.lamcaj@outlook.com>
2022-08-25 22:30:17 -0400

    created findDuplicateChar method on my_util

--- src/my_utils/StringUtil.java
+++ src/my_utils/StringUtil.java
@@ -4,4 +4,29 @@ public class StringUtil {
     /*
         This class will be used to create String utility methods
      */
+
+
+    /*
+    Genti: this method is made to solve the Problem 1: Find duplicate characters from String
+     */
+
+    public static String findDuplicateChar(String str){
+        String duplicate="";
+
+
+        for (int i = 0; i < str.length(); i++) {
+            int count=0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == str.charAt(i)){
+                    count++;
+                }
+
+            }
+            if (count>1 && !duplicate.contains(""+str.charAt(i))){
+                duplicate+= str.charAt(i) + " ";
+            }
+        }
+return duplicate.trim();
+    }
+
 }
