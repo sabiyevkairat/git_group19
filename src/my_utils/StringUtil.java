730d262b9ae93086134f973a62ce969edd4d0ab3
Beyza Gedemenli <beyzagedemenli@gmail.com>
2022-08-26 12:11:03 -0400

    Created and completed countDuplicate method under StringUtil.java

--- src/my_utils/StringUtil.java
+++ src/my_utils/StringUtil.java
@@ -4,4 +4,28 @@ public class StringUtil {
     /*
         This class will be used to create String utility methods
      */
+
+    /*
+    Beyza - created a method to find the duplicate characters and their count in a given string - problem 6
+     */
+    public static void countDuplicate(String word){
+
+        String duplicate = "";
+
+        for(int i = 0; i < word.length(); i++){
+            int count = 0;
+
+            for(int j = 0; j < word.length(); j++){
+                if(word.charAt(i) == word.charAt(j)){
+                    count++;
+                }
+            }
+            if(count > 1 && !duplicate.contains("" + word.charAt(i))){
+                duplicate += word.charAt(i);
+                System.out.println(word.charAt(i) + ": " + count);
+            }
+        }
+
+
+    }
 }
