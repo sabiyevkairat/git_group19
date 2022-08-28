eed17f1401abaa1ba6588010c3ca305693fa3471
Ilana German <ilanagerman95@gmail.com>
2022-08-27 20:18:13 -0400

    Remove duplicate solution is completed

--- src/my_utils/StringUtil.java
+++ src/my_utils/StringUtil.java
@@ -1,7 +1,35 @@
 package my_utils;
 
+import java.util.Arrays;
+
 public class StringUtil {
     /*
         This class will be used to create String utility methods
      */
+    static String removeDuplicate(char str[], int n) {
+
+        int index = 0;
+
+
+        for (int i = 0; i < n; i++)
+        {
+            int j;
+            for (j = 0; j < i; j++)
+            {
+                if (str[i] == str[j])
+                {
+                    break;
+                }
+            }
+            if (j == i)
+            {
+                str[index++] = str[i];
+            }
+        }
+        return String.valueOf(Arrays.copyOf(str, index));
+    }
 }
+
+
+
+
